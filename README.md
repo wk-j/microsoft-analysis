8a795cc9fef1920ebea057d74e6b69c2ac87356b
wk-j <somnuk.wk@gmail.com>
2017-12-13 23:37:14 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## Microsoft.CodeAnalysis.FxCopAnalyzers
+
+- https://github.com/dotnet/roslyn-analyzers
+- https://msdn.microsoft.com/en-us/library/ee1hzekz.aspx
---
+++ src/Hello/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Hello
+{
+    class Person
+    {
+        public int Age { set; get; }
+    }
+
+    // This class violates the rule.
+    public class UseReadOnly
+    {
+        static readonly int x = 3;
+        static readonly double y = x + 2.1;
+        static readonly string s = "readonly";
+    }
+
+
+    class Program
+    {
+        static string s;
+        static void Main(string[] args)
+        {
+            string k = "k";
+            Console.WriteLine(s);
+            Console.WriteLine("Hello World!");
+
+            var p = new Person();
+            p.Age = 0b10100;
+        }
+    }
+}
